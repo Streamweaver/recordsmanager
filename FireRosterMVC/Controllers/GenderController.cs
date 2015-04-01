ea68e1788f5ebd5d3f915dbe5e7188d814874393
Scott Turnbull <scott.turnbull@henrico.us>
2015-04-01 13:47:32 -0400

    Added Gender generic controller and views.

---
+++ FireRosterMVC/Controllers/GenderController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FireRosterMVC.Models;
+
+namespace FireRosterMVC.Controllers
+{
+    public class GenderController : Controller
+    {
+        private FireRosterDB db = new FireRosterDB();
+
+        // GET: Gender
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Gender.ToListAsync());
+        }
+
+        // GET: Gender/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gender gender = await db.Gender.FindAsync(id);
+            if (gender == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gender);
+        }
+
+        // GET: Gender/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gender/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,Label")] Gender gender)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Gender.Add(gender);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(gender);
+        }
+
+        // GET: Gender/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gender gender = await db.Gender.FindAsync(id);
+            if (gender == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gender);
+        }
+
+        // POST: Gender/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,Label")] Gender gender)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gender).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(gender);
+        }
+
+        // GET: Gender/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gender gender = await db.Gender.FindAsync(id);
+            if (gender == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gender);
+        }
+
+        // POST: Gender/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Gender gender = await db.Gender.FindAsync(id);
+            db.Gender.Remove(gender);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

1b74a979e874437eeb1a19caeaf9d531a2ec15cd
Scott Turnbull <scott.turnbull@henrico.us>
2015-04-01 14:09:06 -0400

    Added Race generic controller and views.

---
+++ FireRosterMVC/Controllers/RaceController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FireRosterMVC.Models;
+
+namespace FireRosterMVC.Controllers
+{
+    public class RaceController : Controller
+    {
+        private FireRosterDB db = new FireRosterDB();
+
+        // GET: Race
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Race.ToListAsync());
+        }
+
+        // GET: Race/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Race race = await db.Race.FindAsync(id);
+            if (race == null)
+            {
+                return HttpNotFound();
+            }
+            return View(race);
+        }
+
+        // GET: Race/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Race/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,Label")] Race race)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Race.Add(race);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(race);
+        }
+
+        // GET: Race/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Race race = await db.Race.FindAsync(id);
+            if (race == null)
+            {
+                return HttpNotFound();
+            }
+            return View(race);
+        }
+
+        // POST: Race/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,Label")] Race race)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(race).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(race);
+        }
+
+        // GET: Race/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Race race = await db.Race.FindAsync(id);
+            if (race == null)
+            {
+                return HttpNotFound();
+            }
+            return View(race);
+        }
+
+        // POST: Race/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Race race = await db.Race.FindAsync(id);
+            db.Race.Remove(race);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
